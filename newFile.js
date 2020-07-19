030b40c2d1de2bce65196f4480c32ce766490539
Karenolivia13 <karenolivem13@gmaiil.com>
2020-07-19 21:19:09 +0300

    First assessmemt

---
+++ newFile.js
@@ -0,0 +1,7 @@
+console.log(0.1 + 0.2); //0.3 rounded figure
+console.log(0.1 + 0.2 == 0.3); //false
+console.log("0 || 1 = "+(0 || 1)); // 0 || 1 = 1
+console.log("1 || 2 = "+(1 || 2)); //  1 || 2 = 1
+                   
+console.log("0 && 1 = "+(0 && 1)); // 0 && 1 = 0
+console.log("1 && 2 = "+(1 && 2)); ///1 && 2 = 2
